0849818ba0b8c7feeea39121e629bfe7283cb0ad
Gustavo-Jezini <gustavojezini@gmail.com>
2022-02-13 11:18:55 -0300

    Cria rota post e estrutura funcao create todo. Porem ainda precisa implementar a logica no front end

--- server/controllers/todosController.js
+++ server/controllers/todosController.js
@@ -1,3 +1,27 @@
-export const getTodos = (req, res) => {
-  res.send('ok');
-}
+import PostToDo from "../models/todoModel.js"
+
+export const getTodos = async (req, res) => {
+  try {
+    // Procura no banco de dados (cluster do mongo) se ja tem alguma Tarefa
+    const todos = await PostToDo.find();
+
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(404).json(error.message);
+  };
+};
+
+export const createTodo = async (req, res) => {
+  // Vem do front - end
+  const {title, needTodo, creator} = req.body;
+
+  const newTodo = new PostToDo({title, needTodo, creator})
+
+  try {
+    await newTodo.save()
+
+    res.status(201).json(newTodo)
+  } catch (error) {
+    res.status(409).json({ message: error.message })
+  }
+};
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getTodos } from '../controllers/todosController.js';
+import { createTodo, getTodos } from '../controllers/todosController.js';
 
 const router = express.Router();
 
 router.get('/', getTodos);
+router.post('/', createTodo);
 
 export default router;
